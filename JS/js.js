8d62764c1a36b4b212e2a9668ba5fd046476cdbc
Renato-Chagas-de-Moraes <renatochagas.m@gmail.com>
2023-08-23 23:19:08 -0300

    continuar pag 3

--- JS/js.js
+++ JS/js.js
@@ -1,37 +1,41 @@
-    
 document.addEventListener('DOMContentLoaded', function() {
-    var overlay = document.querySelector('.overlay');
-    var menuTrigger = document.querySelector('.menu-trigger');
-  
-    menuTrigger.addEventListener('click', function() {
-      overlay.style.display = 'flex';
-    });
-  
-    overlay.addEventListener('click', function(event) {
-      if (event.target === overlay) {
-        overlay.style.display = 'none';
-      }
-    });
+  var overlay = document.querySelector('.overlay');
+  var menuTrigger = document.querySelector('.menu-trigger');
+
+  menuTrigger.addEventListener('click', function() {
+    overlay.style.display = 'flex';
   });
 
-//Carrosel
-
-  var radio = document.querySelector('.manual-btn')
-  var cont = 1
-
-document.getElementById('radio1').checked = true
-
-setInterval(() => {
-    proximaImg()
-}, 2000)
-
-function proximaImg(){
-    cont++
-
-    if(cont > 5){
-        cont = 1
+  overlay.addEventListener('click', function(event) {
+    if (event.target === overlay) {
+      overlay.style.display = 'none';
     }
+  });
+});
 
-    document.getElementById('radio'+cont).checked = true
+// Carrosel
+
+var radio = document.querySelector('.manual-btn');
+var cont = 1;
+
+document.getElementById('radio1').checked = true;
+
+setInterval(function() {
+  proximaImg();
+}, 2000);
+
+function proximaImg() {
+  cont++;
+
+  if (cont > 5) {
+    cont = 1;
+  }
+
+  document.getElementById('radio' + cont).checked = true;
 }
 
+
+document.addEventListener('DOMContentLoaded', function() {
+  var slide = document.querySelector('.slide');
+  slide.classList.add('animated', 'slide-in');
+});
